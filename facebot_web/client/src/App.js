181fd5fae9bd4137bd99f15d0b02a9fc46541f2d
nairvilchis <nairvilchis@gmail.com>
2025-06-14 21:26:13 +0000

    add app.js

---
+++ facebot_web/client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Flow from './components/Flow';
+
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Flow />
+    </div>
+  );
+}
+
+export default App;
